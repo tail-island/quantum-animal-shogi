a847ea3684ff3cb647ba8249e9173c7315b64415
Ojima Ryoji <ryoji.ojima@biprogy.com>
2026-04-16 11:58:36 +0900

    リファクタリング。

--- quantum-animal-shogi/_b.ps1
+++ quantum-animal-shogi/_b.ps1
@@ -5,6 +5,8 @@ try {
         Set-Location -Path crates\python
         maturin build --release
         Copy-Item ..\..\target\wheels\quantum_animal_shogi-0.1.0-cp313-cp313-win_amd64.whl ..\..\..\dist
+    } catch {
+        throw
     } finally {
         Set-Location ..\..
     }
@@ -12,6 +14,8 @@ try {
     try {
         Set-Location -Path crates\wasm
         wasm-pack build --release
+    } catch {
+        throw
     } finally {
         Set-Location -Path ..\..
     }
--- quantum-animal-shogi/_t.ps1
+++ quantum-animal-shogi/_t.ps1
@@ -5,6 +5,8 @@ try {
         Set-Location -Path crates\python
         maturin develop --release
         python ..\..\_t.py
+    } catch {
+        throw
     } finally {
         Set-Location -Path ..\..
     }
