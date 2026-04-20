e5564048b86780374d66340d888e7c59a3f2dcf1
Ojima Ryoji <ryoji.ojima@biprogy.com>
2026-04-21 08:59:49 +0900

    デバッグ・コードを追加

--- reinforcement-learning/alpha_zero_general/Coach.py
+++ reinforcement-learning/alpha_zero_general/Coach.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from .Arena import Arena
 from .MCTS import MCTS
 
+
 log = logging.getLogger(__name__)
 
 
@@ -64,6 +65,10 @@ class Coach():
             board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
 
             r = self.game.getGameEnded(board, self.curPlayer)
+
+            if np.all(board.observe()["action_mask"] == 0):
+                for x in trainExamples:
+                    print(x[0].render())
 
             if r != 0:
                 return [(x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer))) for x in trainExamples]
